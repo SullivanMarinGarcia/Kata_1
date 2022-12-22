d612945c4672a337963309efa20c0a595c0beabe
sgarc <sgarc@Sullivan_Laptop>
2022-12-22 17:56:47 +0100

    Segunda versión de la Kata 1

--- src/kata_1/Person.java
+++ src/kata_1/Person.java
@@ -1,12 +1,16 @@
 package kata_1;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+
 
 public class Person {
     private final String name;
-    private final Date birthdate;
+    private final Calendar birthdate;
+    private final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
 
-    public Person(String name, Date birthdate) {
+    public Person(String name, Calendar birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -15,14 +19,21 @@ public class Person {
         return name;
     }
 
-    public Date getBirthdate() {
+    public Calendar getBirthdate() {
         return birthdate;
     }
     
     
     public int getAge(){
-        return (int) ((new Date().getTime() - birthdate.getTime()) / 31557600000L);
+        Calendar today = GregorianCalendar.getInstance();
+        return (int) (milliSecondsToYear(today.getTimeInMillis() - birthdate.getTimeInMillis()));
     }
+    
+    
+    private long milliSecondsToYear(long milles){
+        return milles / MILLISECONDS_PER_YEAR;
+    }
+    
     
     
     
